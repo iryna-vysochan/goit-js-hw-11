17d1b49379a738cf20cac006b4514d3485c57ce5
iryna-vysochan <ivysochan@gmail.com>
2025-11-01 18:33:06 +0200

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -5,7 +5,7 @@ export default function getImagesByQuery(query) {
   return axios
     .get('https://pixabay.com/api/', {
                     params: {
-                      key: '52124620-04c3728bcdfdd891621e81587',
+                      key: '53047791-da6bf0178b39a3cb2727ecc21',
                       q: query,
                       image_type: "photo",
                       orientation: 'horizontal',
